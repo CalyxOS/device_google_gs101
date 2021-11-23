73757f1258a0bfa48021660153a5f8d5867352c9
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-23 22:32:25 +0530

    Build vendor_dlkm
    Change-Id: Ic2a0e4cdfdcc448a73bef41e0d2140e9d0aefcf0

--- BoardConfig-calyx.mk
+++ BoardConfig-calyx.mk
@@ -3,3 +3,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
+AB_OTA_PARTITIONS += vendor_dlkm
+
+# vendor_dlkm.img
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
