df51d57728d354c4b27af6899dda3dd28392c628
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-05 05:25:36 +0530

    Build vendor
    Change-Id: I08dff42223ae54386a5ed050b51aa4a39e08bdb6

--- BoardConfig-calyx.mk
+++ BoardConfig-calyx.mk
@@ -5,7 +5,12 @@
 #
 
 AB_OTA_PARTITIONS += vendor_dlkm
+AB_OTA_PARTITIONS += vendor
 
 # vendor_dlkm.img
 BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+
+# vendor.img
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
