6d281b0b3688b24571026c1a593f96c0c3d9bf46
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-05 05:25:36 +0530

    Build vendor
    Change-Id: I08dff42223ae54386a5ed050b51aa4a39e08bdb6

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -105,6 +105,7 @@ AB_OTA_PARTITIONS += vbmeta
 endif
 
 AB_OTA_PARTITIONS += vendor_dlkm
+AB_OTA_PARTITIONS += vendor
 
 # EMULATOR common modules
 BOARD_EMULATOR_COMMON_MODULES := liblight
@@ -181,6 +182,10 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 # vendor_dlkm.img
 BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+
+# vendor.img
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
 
 ########################
 # Video Codec
