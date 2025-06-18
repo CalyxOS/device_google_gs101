c734c46ceed3d2918a3074e75845e58de6289ce0
Michael Bestas <mkbestas@gmail.com>
2025-06-19 02:37:57 +0300

    gs101: Remove non-existent vendor includes
    Change-Id: I1bc76430d30259984f4eb91907be61ef39646c27

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -399,6 +399,4 @@ BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 # Protected VM firmware
 BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
 
--include vendor/google_devices/gs-common/proprietary/BoardConfigVendor.mk
-
 include device/google/gs101/BoardConfig-calyx.mk
