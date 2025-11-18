Tortel/android_device_google_gs101

gs101: Update sepolicy path

sepolicy/gs101-sepolicy.mk
@@ -1,21 +1,21 @@
 # sepolicy that are shared among devices using whitechapel
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/whitechapel/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/whitechapel/vendor/google
 
 # unresolved SELinux error log with bug tracking
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/tracking_denials
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/private
 
 # Display
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/display/common
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/display/gs101
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/display/common
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/display/gs101
 
 # Micro sensor framework (usf)
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/usf
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/usf
 
 # system_ext
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/private
 
 #
 # Pixel-wide
@@ -39,4 +39,4 @@ BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_sniffer
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/wifi_logger
 
 # Public
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101-sepolicy/public
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/public

CalyxOS/device_google_gs101

Change-Id: I4bcee50514c02294137ea5ac6e64c64d69308bd4

sepolicy/gs101-sepolicy.mk
@@ -5,20 +5,20 @@ BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/input
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
 
 # sepolicy that are shared among devices using whitechapel
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs101-sepolicy/whitechapel/vendor/google
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs101/sepolicy/whitechapel/vendor/google
 
 # unresolved SELinux error log with bug tracking
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/tracking_denials
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/tracking_denials
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/private
 
 # Display
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/display/common
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/display/gs101
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/display/common
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/display/gs101
 
 # system_ext
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101/sepolicy/system_ext/private
 
 #
 # Pixel-wide
@@ -27,12 +27,12 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs101-sepolicy/system_ext/priv
 BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 
 # Public
-PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101-sepolicy/public
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs101/sepolicy/public
 
 # pKVM
 ifeq ($(TARGET_PKVM_ENABLED),true)
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/pkvm
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/pkvm
 endif
 
 # Health HAL
-BOARD_SEPOLICY_DIRS += device/google/gs101-sepolicy/health
+BOARD_SEPOLICY_DIRS += device/google/gs101/sepolicy/health